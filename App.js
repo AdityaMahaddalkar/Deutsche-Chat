de0130668aef22bb6c6abd0932f1d364e2766d04
Aditya Mahaddalkar <adityam1311@gmail.com>
2021-02-26 15:07:34 +0530

    Added image and changed UI elements

--- App.js
+++ App.js
@@ -1,15 +1,18 @@
 import { NavigationContainer, StackActions } from "@react-navigation/native";
 import React from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View, Image } from "react-native";
 import { Header } from "react-native-elements";
 import DeutscheChat from "./components/deutsche-chat";
 import { createStackNavigator } from "@react-navigation/stack";
 import { FAB, Headline } from "react-native-paper";
+import { Avatar } from "react-native-paper";
 
 function HomeScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Headline>Welcome to the Form Bot</Headline>
+    <View style={styles.view}>
+      <Headline>Welcome to the FinnoBot</Headline>
+      <Avatar.Image size={180} source={require("./assets/chatbot.jpg")} />
+
       <FAB
         style={styles.fab}
         label="Continue to Form"
@@ -25,7 +28,16 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen}></Stack.Screen>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            title: "Home",
+            headerStyle: {
+              backgroundColor: "#45B39D",
+            },
+          }}
+        ></Stack.Screen>
         <Stack.Screen name="Form" component={DeutscheChat}></Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
@@ -37,10 +49,18 @@ const styles = StyleSheet.create({
     borderTopWidth: 30,
     fontSize: 14,
   },
+  view: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 10,
+  },
   fab: {
     position: "absolute",
+    backgroundColor: "#45B39D",
     margin: 16,
     right: 0,
     bottom: 0,
   },
+  image: {},
 });
